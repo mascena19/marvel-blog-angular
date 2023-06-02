e50d99712e10551341853ebef96db050a5b4bd5c
masce <mascena19@gmail.com>
2023-06-02 01:40:48 -0300

    Projeto Blog com angular

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+    "description": "Marvel Anúncia novo filme do homem de ferro",
+    "photoCover": "https://colunaboraviajar.com.br/wp-content/uploads/2023/05/Cena-deletada-de-Iron-Man-e-tao-vergonhosa-que-nunca-sera-lancada-diz-produtor.jpg"
+  },
+  {
+    "id": "2",
+    "title": "NOVA Série Marvel",
+    "description": "Marvel Anúncia novo série",
+    "photoCover": "https://files.tecnoblog.net/wp-content/uploads/2020/11/ordem-marvel-e1606754420868-700x393.jpg"
+  },
+   {
+    "id": "3",
+    "title": "NOVA Série Cavaleiro da Lua",
+    "description": "Série cavaleiro da lua lançada",
+    "photoCover": "https://kanto.legiaodosherois.com.br/w760-h398-cfill/wp-content/uploads/2022/10/legiao_D8mFjTNqpZY5.png.webp"
+  },
+  {
+    "id": "4",
+    "title": "Nova Série Dr. Estranho",
+    "description": "Série Dr. Estranho lançada",
+    "photoCover": "https://mundoconectado.com.br/uploads/chamadas/doutor-estranho-no-multiverso-da-loucura-imagem-reproducao-marvel-studios.jpg"
+  },
+  {
+    "id": "5",
+    "title": "Goiás Campeão",
+    "description": "Goiás campeão da copa Verde 2023",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW1PaiRut3_SzJnFIQo1xB_ESMH-8Urj_39Q&usqp=CAU"
+  }
+]
